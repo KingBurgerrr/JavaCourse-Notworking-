214ae236a23a4893be10a2308c304b10e1efdd61
KingBurgerrr <61909192+KingBurgerrr@users.noreply.github.com>
2020-07-31 17:14:40 +0300

    Abstyrations

---
+++ abstract/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(9,12);
+        System.out.println("Поле прямокутника(S = a * b) =" + rect.square());
+    }
+}
